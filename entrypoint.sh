15835aad5ff322aef34eb9d189e40838a53625ec
Juan Carlos Mejías Rodríguez <juanmejias231@gmail.com>
2021-07-07 01:53:21 -0400

    Use "origin" remote

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-merge_commits=$(git rev-list --merges "$GITHUB_BASE_REF".."$GITHUB_HEAD_REF")
+merge_commits=$(git rev-list --merges "origin/$GITHUB_BASE_REF".."origin/$GITHUB_HEAD_REF")
 
 if [ -n "$merge_commits" ]; then
   echo "Error: merge commits found in $GITHUB_BASE_REF..$GITHUB_HEAD_REF"
